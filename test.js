57e8c0d72c526d0ef505875352914bd0d2670e07
Equinn Nightfury <xianzyip8@gmail.com>
2021-11-27 13:09:36 +0800

    OK3

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-function scrollFunction() {
-    if (document.documentElement.scrollTop < 200) {
-        document.getElementsByClassName('first')[0].style.display = 'block';
-        document.getElementsByClassName('second')[0].style.display = 'none';
-    } else if (document.documentElement.scrollTop < 400) {
-        document.getElementsByClassName('first')[0].style.display = 'none';
-        document.getElementsByClassName('second')[0].style.display = 'block';
-    }
-}
 
-window.onscroll = scrollFunction;
